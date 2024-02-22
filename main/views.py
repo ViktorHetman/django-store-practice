edb091eaeac1e378eb27d05a14e50c4622ffcc02
Viktor <vitya.getman.79@gmail.com>
2024-02-22 19:44:16 +0100

    first template

--- main/views.py
+++ main/views.py
@@ -4,7 +4,15 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Home page')
+    context = {
+        'title': 'Home',
+        'content': "The main store's page",
+        'list': ['first', 'second'],
+        'dict': {'first': 1},
+        'is_authenticated': False,
+    }
+
+    return render(request, 'main/index.html', context)
 
 
 def about(request):
