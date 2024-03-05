78bf5c4f261f031a64743b6f9496f4e5661395d6
Viktor <vitya.getman.79@gmail.com>
2024-03-05 16:47:41 +0100

    reworked getting data in html files

--- main/views.py
+++ main/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
+
+from goods.models import Categories
 
 
 def index(request):
+
+    categories = Categories.objects.all()
+
     context = {
         'title': 'Home - Главная',
-        'content': 'Магазин мебели HOME'
+        'content': 'Магазин мебели HOME',
+        'categories': categories
     }
 
     return render(request, 'main/index.html', context)
